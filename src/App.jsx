8c848803f468ae45dc405bca75daf0fd27a786b6
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-10-02 10:39:00 -0400

    Routes updated to apply Header to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
@@ -6,10 +7,12 @@ import About from "./pages/About";
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Route>
+      </Routes>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
 
     return (
-        <header className="sticky top-5 pr-5 z-50 flex justify-end w-full">
+        <header className="sticky top-5 pr-5 z-50 w-full">
             {/* <nav className="max-w-6xl mx-auto px-4 py-4 flex justify-center items-center"> */}
               {/* <nav className={`inline-flex items-center space-x-10 rounded-xl transition-all duration-300 ${scrolled ? "bg-white shadow-md px-8 py-4" : "bg-transparent px-8 py-4"}`}> */}
             {/* <nav className={`inline-flex items-center space-x-10 rounded-xl transition-all duration-300`}> */}
@@ -9,7 +9,6 @@ const Header = () => {
                 <ul>
                     <li><a>Home</a></li>
                     <li><a>About</a></li>
-
                 </ul>
             </nav>
         </header>
