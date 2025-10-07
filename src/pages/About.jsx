f5950b17ba0d3a1a8a0d83c3b64e311582cabe49
Mastracci <38020285+MastracciN@users.noreply.github.com>
2025-10-07 13:32:45 -0400

    added profile picture to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import profile from '../assets/profile.jpg'
+
 const About = () => {
 
     return (
@@ -5,10 +7,12 @@ const About = () => {
             <h1>About</h1>
             <div className="flex">
                 <aside className="flex flex-col">
-                    <p>image</p>
+                    <img src={profile} alt="Profile Picture" 
+                        className="w-50 md:w-56 lg:w-70 xl:w-70 aspect-square rounded-full object-cover mb-6 lg:mb-10 
+                        transition-transform duration-300 hover:scale-102"/>
                 </aside>
                 <main>
-                    hey
+                    <p>hey</p>
                 </main>
             </div>
         </div>
