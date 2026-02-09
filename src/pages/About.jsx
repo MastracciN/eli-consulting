50c4e1554a2f2bd04492698a81dc5384f9491406
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-02-08 21:09:39 -0500

    about layout sections added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,30 +6,56 @@ const About = () => {
         <div className="max-w-[1200px] p-4 mx-auto">
             <h1 className="text-center text-5xl m-12 font-bold">About Elijah</h1>
 
-            <div className="flex flex-col md:flex-row justify-between items-center">
-                <aside className="w-1/2 flex justify-center items-center">
-                    <img src={profile} 
-                        alt="Profile Picture" 
-                        className="w-50 md:w-56 lg:w-70 xl:w-70 aspect-square rounded-full object-cover mb-12 md:mb-6 lg:mb-10"/>
-                </aside>
-                <main className="w-full md:w-1/2 flex-col justify-center items-center">
-                    <p className=''>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-                        officia deserunt mollit anim id est laborum.
-                    </p>
-                    <br/>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-                        officia deserunt mollit anim id est laborum.
-                    </p>
-                </main>
-            </div>
+            <main className='flex flex-col space-y-10'>
+                <div className='flex flex-col md:flex-row justify-between items-center'>
+                    <div className="w-1/2 flex justify-center items-center">
+                        <img src={profile} 
+                            alt="Profile Picture" 
+                            className="w-50 md:w-56 lg:w-70 xl:w-70 aspect-square rounded-full object-cover mb-12 md:mb-6 lg:mb-10"/>
+                    </div>
+                    <div className="w-full md:w-1/2 flex-col justify-center items-center space-y-4">
+                        <h2 className='text-3xl font-bold'>Heading</h2>
+                        <p className=''>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                            officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                            officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex flex-col md:flex-row justify-between items-center space-y-4'>
+                    <div className="w-full md:w-1/2 flex-col justify-center items-center space-y-4">
+                        <h2 className='text-3xl font-bold'>Heading</h2>
+                        <p className=''>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                            officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                            officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <div className="w-1/2 flex justify-center items-center">
+                        <img src={profile} 
+                            alt="Profile Picture" 
+                            className="w-50 md:w-56 lg:w-70 xl:w-70 aspect-square rounded-full object-cover mb-12 md:mb-6 lg:mb-10"/>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
