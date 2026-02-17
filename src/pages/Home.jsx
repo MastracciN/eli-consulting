c398a69c1494dd80bec8a59fa8db9489afcb1053
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-02-17 09:42:15 -0500

    added hero section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 const Home = () => {
 
     return (
-        <p>Home</p>
+        <div>
+            {/* Hero */}
+            <section id='hero' className='relative bg-cover bg-center'>
+                <div className='absolute inset-0 bg-black/50'></div>
+
+                <div className='relative min-h-[75vh] flex items-center'>
+                    <div className='w-full max-w-[1200px] mx-auto'>
+                        <h1 className="text-5xl font-bold">Quote</h1>
+                    </div>
+                </div>
+
+            </section>
+        </div>
     );
 };
 
