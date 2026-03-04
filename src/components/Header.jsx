05125ae630e65ed6c766aa214d96ff236f6aa591
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-03-04 13:23:19 -0500

    header link underlines match

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,14 +22,11 @@ export default function Header() {
                             to={path} 
                             end={end}
                             className={({ isActive }) =>
-                                `pb-1 border-b-2 ${
-                                    isActive
-                                    ? "border-blue-500"
-                                    : `border-transparent relative inline-block 
-                                       after:absolute after:left-0 after:bottom-0 
-                                       after:h-[1px] after:w-0 after:bg-blue-600 
-                                       after:transition-all after:duration-300 hover:after:w-full`
-                                }`
+                                `relative inline-block pb-1
+                                after:absolute after:left-0 after:bottom-0
+                                after:h-[1px] after:bg-blue-600
+                                after:transition-all after:duration-300
+                                ${isActive ? "after:w-full" : "after:w-0 hover:after:w-full"}`
                             }
                         >
                             {name}
