21fe03c1356591a3537de4698c2e518710945edb
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-03-06 13:09:58 -0500

    header link highlight changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
                             className={({ isActive }) =>
                                 `relative inline-block pb-1
                                 after:absolute after:left-0 after:bottom-0
-                                after:h-[2px] after:bg-blue-600
+                                after:h-[2px] after:bg-dg
                                 after:transition-all after:duration-300
                                 ${isActive ? "after:w-full font-bold" : "after:w-0 hover:after:w-full"}`
                             }
