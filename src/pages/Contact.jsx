544639328b8aae512d50e4b96ad225d075f60b8d
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-03-30 10:53:30 -0400

    updated hero section on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,5 @@
+import rockyriver from '../assets/rockyriver.jpg'
+
 const Contact = () => {
 
     const handleSubmit = async (e) => {
@@ -17,14 +19,22 @@ const Contact = () => {
     }
 
     return (
-        <div>
+        <div className="bg-main">
 
-            <section id='hero' className='relative bg-cover bg-center'>
-                <div className='absolute inset-0 bg-black/50'></div>
-                <div className='h-[350px] flex items-center justify-center'>
-                    <h1 className="text-center text-5xl font-bold">Contact</h1>
+            <section 
+                id='hero' 
+                className='relative bg-cover bg-center px-6'
+                style={{ backgroundImage: `url(${rockyriver})` }}    
+            >
+                <div className='absolute inset-0 bg-black/75'></div>
+
+                <div className='relative min-h-[40vh] flex justify-center text-white text-center items-center'>
+                        <h3 className="text-4xl md:text-6xl font-serif drop-shadow-lg py-8 md:p-0 font-bold max-w-5xl">
+                            Contact Us
+                        </h3>
                 </div>
             </section>
+
 
             <section id='contact' className="w-full">
                 <div className='w-full max-w-[1200px] mx-auto px-6 py-12 flex flex-col md:flex-row'>
