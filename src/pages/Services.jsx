4fa9a26af7663edbfd434f09583b53c1cd56fbfe
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-03-30 07:42:50 -0400

    updated values section layout on service page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -194,76 +194,77 @@ const Services = () => {
                         </div>
                     </div>
 
-                    {/* <div className='flex flex-col md:flex-row mt-5 md:space-x-8 space-y-8 md:space-y-0 max-w-[1400px] mx-auto w-full'>
-                        <div className='flex flex-col flex-1 space-y-2 p-8 rounded-lg transition-all duration-300 bg-crm hover:bg-black/10 hover:shadow-xl border-1 border-dg/50'>
-                            <p className='text-2xl font-serif'>Reconciliation Strategy & Organizational Advisory</p>
-                            <p>Supporting organizations committed to meaningful change.</p>
-                            <p>Services include:</p>
-                            <ul className='list-disc pl-5'>
-                                <li>Development of Indigenous pillars and reconciliation frameworks</li>
-                                <li>Organizational assessments and strategic guidance</li>
-                                <li>Aligning initiatives with TRC Calls to Action</li>
-                                <li>Leadership advisory on Indigenous inclusion and belonging</li>
-                            </ul>
-                            <p>This work moves organizations beyond intention toward measurable action.</p>
-                        </div>
-                        
-                        <div className='flex flex-col flex-1 space-y-2 p-8 rounded-lg transition-all duration-300 bg-crm hover:bg-black/10 hover:shadow-xl border-1 border-dg/50'>
-                            <p className='text-2xl font-serif'>Community Relationship Building</p>
-                            <p>Helping organizations build respectful and lasting relationships with Indigenous communities.</p>
-                            <p>Services include:</p>
-                            <ul className='list-disc pl-5'>
-                                <li>Guidance on ethical community engagement</li>
-                                <li>Connecting organizations with First Nation communities</li>
-                                <li>Partnership development rooted in reciprocity</li>
-                                <li>Land acknowledgement development grounded in meaningful action</li>
-                            </ul>
-                            <p>Reconciliation grows through relationship, not transactions.</p>
-                        </div>
-
-                    </div> */}
-
                 </section>
 
-                <section id='values' className='max-w-[1400px] mx-auto m-20 px-6 md:px-0'>
-                    <h2 className='text-5xl font-serif'>Our Values</h2>
-                    <h3 className='text-3xl font-serif pt-2'>Guided by the Seven Grandfather Teachings</h3>
-                    <p className='pt-2'>The work of Blackbird Bimaadiziwin Consulting is guided by teachings that have guided Anishinaabe people for generations.</p>
-                    <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 mt-8 justify-center items-center'>
-                        <div className='p-4 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px] '>
-                            <h4>Wisdom (Nbwaakaawin)</h4>
-                            <p>Using knowledge and experience to guide decisions that benefit the community.</p>
+                <section id='values' className='mx-auto m-20 px-6 md:px-0'>
+
+                    {/* <div className='relative bg-dg text-white py-10 overflow-hidden'>
+                        <div 
+                            className="absolute inset-0 opacity-2 bg-repeat z-0"
+                            alt='Wood pattern'
+                            style={{ backgroundImage: `url(${woodpattern})` }}    
+                        ></div>
+                        <div className='flex flex-col md:flex-row mx-auto items-center space-x-20 justify-center'>
+                            <h3 className='text-6xl drop-shadow-5xl font-serif'>Our Values</h3>
+                            <div className='max-w-[600px]'>
+                                <h3 className='text-3xl font-serif pt-2'>Guided by the Seven Grandfather Teachings</h3>
+                                <p className='text-xl pt-2 text-white/90'>The work of Blackbird Bimaadiziwin Consulting is guided by teachings that have guided Anishinaabe people for generations.</p>
+                            </div>
                         </div>
-                        <div className='p-4 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px]'>
-                            <h4>Love (Zaagi’idiwin)</h4>
-                            <p>Approaching this work with compassion and care for people and relationships.</p>
+                    </div> */}
+                    <div className='flex flex-col md:flex-row max-w-[1400px] mx-auto justify-center md:space-x-12 items-center'>
+                        <div className='max-w-[500px] items-center'>
+                            {/* <div className='border-l-2 border-dg/510 pl-6'> */}
+                            <h2 className='text-6xl font-serif'>Our Values</h2>
+                            <h3 className='text-4xl font-serif pt-2 text-black/90 mt-4'>Guided by the Seven Grandfather Teachings</h3>
+                            <p className='mt-6'>The work of Blackbird Bimaadiziwin Consulting is guided by teachings that have guided Anishinaabe people for generations.</p>
+                            <p className='mt-6'>These teachings guide how we walk alongside organizations and communities.</p>
+                            {/* </div> */}
                         </div>
-                        <div className='p-4 md:py-7 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px]'>
-                            <h4>Respect (Mnaadendimowin)</h4>
-                            <p>Honouring the dignity, knowledge, and perspectives of all people.</p>
+
+                        {/* <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 mt-8 justify-center items-center'> */}
+                        <div className=''>
+                            <div className='p-4 mt-4 max-w-[300px] border-l-1 border-dg/70 
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Wisdom (Nbwaakaawin)</h4>
+                                <p>Using knowledge and experience to guide decisions that benefit the community.</p>
+                            </div>
+                            <div className='p-4 mt-8 max-w-[300px] border-l-1 border-dg/70
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Love (Zaagi’idiwin)</h4>
+                                <p>Approaching this work with compassion and care for people and relationships.</p>
+                            </div>
+                            <div className='p-4 mt-8 max-w-[300px] border-l-1 border-dg/70
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Respect (Mnaadendimowin)</h4>
+                                <p>Honouring the dignity, knowledge, and perspectives of all people.</p>
+                            </div>
+                            <div className='p-4 mt-8 max-w-[300px] border-l-1 border-dg/70
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Bravery (Aakode’ewin)</h4>
+                                <p>Having the courage to speak truth and engage in difficult conversations.</p>
+                            </div>
                         </div>
-                        <div className='p-4 md:py-7 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px]'>
-                            <h4>Bravery (Aakode’ewin)</h4>
-                            <p>Having the courage to speak truth and engage in difficult conversations.</p>
+
+                        {/* <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 mt-2 md:mt-8 justify-center items-center'> */}
+                        <div className=''>
+                            <div className='p-4 mt-8 max-w-[300px] border-l-1 border-dg/70
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Honesty (Gwekwaadziwin)</h4>
+                                <p>Walking with integrity and openness in all relationships.</p>
+                            </div>
+                            <div className='p-4 mt-8 max-w-[300px] border-l-1 border-dg/70
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Humility (Dabaadendiziwin)</h4>
+                                <p>Recognizing that we are all part of something greater than ourselves.</p>
+                            </div>
+                            <div className='p-4 mt-8 max-w-[300px] border-l-1 border-dg/70
+                                transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:bg-crm'>
+                                <h4 className='text-xl font-serif'>Truth (Debwewin)</h4>
+                                <p>Seeking understanding through listening, reflection, and shared learning.</p>
+                            </div>
                         </div>
                     </div>
-
-                    <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 mt-2 md:mt-8 justify-center items-center'>
-                        <div className='p-4 md:py-7 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px]'>
-                            <h4>Honesty (Gwekwaadziwin)</h4>
-                            <p>Walking with integrity and openness in all relationships.</p>
-                        </div>
-                        <div className='p-4 md:py-7 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px]'>
-                            <h4>Humility (Dabaadendiziwin)</h4>
-                            <p>Recognizing that we are all part of something greater than ourselves.</p>
-                        </div>
-                        <div className='p-4 text-center rounded-lg bg-crm border-1 border-dg/50 max-w-[300px]'>
-                            <h4>Truth (Debwewin)</h4>
-                            <p>Seeking understanding through listening, reflection, and shared learning.</p>
-                        </div>
-                    </div>
-
-                    <p className='mt-8'>These teachings guide how we walk alongside organizations and communities.</p>
                 </section>
 
                 <ContactSection/>
