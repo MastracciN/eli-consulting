58c818c9ca844806eecbc5361fb19665246e2d5a
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-03-31 07:24:36 -0400

    added info section with links to site pages in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <footer className="py-16 min-h-[250px] bg-text text-white">
             <div className="max-w-[1400px] mx-auto flex flex-col md:flex-row">
-                <div id="left-col" className="flex-2">
+                <div className="flex-2">
                     <h2 className="sr-only">Blackbird Bimaadiziwin Consulting</h2>
                     <img
                         src={logo}
@@ -14,8 +14,49 @@ export default function Footer() {
                         className="h-12 w-auto md:h-18"
                     />
                 </div>
-                <div id="right-col" className="flex flex-1 flex-col items-start space-y-1">
-                    <h2 className="text-2xl font-bold mb-6">Contact</h2>
+
+                <div className="flex flex-1 flex-col space-y-1">
+                    <h2 className="text-2xl font-serif mb-6">Info</h2>
+                    <NavLink 
+                        to='/home'
+                        className="relative inline-block w-fit
+                            after:absolute after:left-0 after:bottom-[1px]
+                            after:h-[1px] after:w-full
+                            after:origin-left
+                            after:scale-x-0
+                            after:bg-current
+                            after:transition-transform after:duration-300
+                            hover:after:scale-x-100">
+                        Home
+                    </NavLink>
+                    <NavLink 
+                        to='/about'
+                        className="relative inline-block w-fit
+                            after:absolute after:left-0 after:bottom-[1px]
+                            after:h-[1px] after:w-full
+                            after:origin-left
+                            after:scale-x-0
+                            after:bg-current
+                            after:transition-transform after:duration-300
+                            hover:after:scale-x-100">
+                        About
+                    </NavLink>
+                    <NavLink 
+                        to='/services'
+                        className="relative inline-block w-fit
+                            after:absolute after:left-0 after:bottom-[1px]
+                            after:h-[1px] after:w-full
+                            after:origin-left
+                            after:scale-x-0
+                            after:bg-current
+                            after:transition-transform after:duration-300
+                            hover:after:scale-x-100">
+                        Services
+                    </NavLink>
+                </div>
+
+                <div className="flex flex-1 flex-col items-start space-y-1">
+                    <h2 className="text-2xl font-serif font-medium mb-6">Contact</h2>
                     <NavLink 
                         to='/contact'
                         className="relative inline-block
@@ -26,7 +67,7 @@ export default function Footer() {
                             after:bg-current
                             after:transition-transform after:duration-300
                             hover:after:scale-x-100">
-                        Contact Me
+                        Contact
                     </NavLink>
                     <a 
                         href="https://www.linkedin.com/in/elijah-blackbird-he-him-390374206/"
