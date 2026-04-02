38937c972973d298019883bffafba07470da533d
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-04-02 12:54:21 -0400

    contact section text and button styling updated

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -12,30 +12,31 @@ export default function ContactSection() {
 
             <div className='relative flex items-center'>
                 <div className='w-full max-w-[1400px] mx-auto flex flex-col md:flex-row space-y-4 md:space-y-0 py-8 md:py-0 items-center justify-between'>
-                    <div className=" px-6 md:px-0 flex flex-col text-white space-y-2 mb-8 md:mb-0 drop-shadow-2xl">
+                    <div className=" px-6 lg:px-0 flex flex-col text-white mb-8 md:mb-0 drop-shadow-2xl">
                         <h2 className="text-5xl font-bold text-white font-serif mb-5">Begin the Journey</h2>
-                        <p className="text-xl">
-                            Reconciliation is not a single event or workshop.
-                        </p>
-                        <p className="text-xl">
-                            It is a long-term commitment to relationship, learning, and shared responsibility.
-                        </p>
-                        <p className="text-xl">
-                            If your organization is ready to walk alongside Indigenous communities in a meaningful way, we welcome that journey together.
-                        </p>
+                        <div className="flex flex-col text-xl space-y-2">
+                            <p className="text-white/90">
+                                Reconciliation is not a single event or workshop.
+                            </p>
+                            <p className="text-white">
+                                It is a long-term commitment to relationship, learning, and shared responsibility.
+                            </p>
+                            <p className="text-white/90">
+                                If your organization is ready to walk alongside Indigenous communities in a meaningful way, we welcome that journey together.
+                            </p>
+                        </div>
                     </div>
                     <NavLink
                         to='/contact'
-                        className={({ isActive }) =>
-                            `px-6 py-3 rounded-lg font-semibold transition ${
-                            isActive
-                                ? 'bg-black text-white'
-                                : 'border border-white text-white hover:bg-white/20 hover:text-white'
-                            }`
-                        }
+                        className='inline-block px-10 py-4 text-white font-medium rounded-lg bg-gray-950
+                            shadow-md transition-all duration-300
+                            hover:bg-black hover:shadow-lg hover:-translate-y-0.5
+                            focus:outline-none focus:ring-2 focus:ring-dg/50'
                     >
-                        Contact
+                        Contact   
                     </NavLink>
+
+                    
                 </div>
             </div>
         </section>
