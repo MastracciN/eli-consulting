44ec9d586a091975c7e486b1751342a71d04cb3b
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-04-03 14:43:34 -0400

    implemented formspree to manage messages from website

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,21 +3,7 @@ import rockyriver from '../assets/rockyriver.jpg'
 
 const Contact = () => {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.target);
-
-        const data = {
-            name: formData.get("name"),
-            email: formData.get("email"),
-            message: formData.get("message")
-        };
-
-        console.log("Submitted:", data);
-
-        e.target.reset();
-    }
+    const [state, handleSubmit] = useForm('xqegybrp')
 
     return (
         <div className="bg-main">
@@ -44,19 +30,6 @@ const Contact = () => {
                         <div className='md:pl-8 py-4 md:border-l-2 md:border-dg/50'>
                             <h2 className='text-4xl md:text-5xl font-serif'>Get in touch</h2>
                             <p className='mt-4'>If you would like to work with us or learn more, don't hesitate to send a message. We would love to hear from you.</p>
-                            {/* <a 
-                                href="https://www.linkedin.com/in/elijah-blackbird-he-him-390374206/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="relative inline-block text-blue-600
-                                        after:absolute after:left-0 after:bottom-0
-                                        after:h-[1px] after:w-0
-                                        after:bg-blue-600
-                                        after:transition-all after:duration-300
-                                        hover:after:w-full"
-                            >
-                                https://www.linkedin.com/in/elijah-blackbird-he-him-390374206/
-                            </a> */}
                         </div>
                         <div className='flex mt-8 ml-8'>
                             <a 
@@ -80,8 +53,17 @@ const Contact = () => {
 
                     <div className="flex-1 md:pl-8 lg:pl-0">
                         <h2 className="text-4xl md:text-5xl font-serif mb-8 mt-10 lg:mt-0">Send us a message</h2>
+
+                        {state.succeeded && (
+                            <p className="text-green-600 mb-4">
+                                Thanks! Your message has been sent.
+                            </p>
+                        )}
+                        
                         <form
                             onSubmit={handleSubmit}
+                            action="https://formspree.io/f/xqegybrp"
+                            method="POST"
                             className="space-y-6 max-w-[600px]"
                         >
                             <div>
@@ -91,10 +73,11 @@ const Contact = () => {
                                     required
                                 />
                                 <label className="block mb-1 mt-6 font-medium">Email</label>
-                                <input type='text' name='email' placeholder='jsmith@email.ca'
+                                <input type='email' name='email' placeholder='jsmith@email.ca'
                                     className="w-full border-b-2 border-dg/50 px-4 py-3 focus:outline-none focus:ring-2 focus:rounded-lg focus:ring-black"
                                     required
-                                /> 
+                                />
+                                <ValidationError prefix='Email' field='email' errors={state.errors} /> 
                                 <label className="block mb-1 mt-6 font-medium">Message</label>
                                 <textarea 
                                     name='message'
@@ -103,12 +86,15 @@ const Contact = () => {
                                     placeholder='I would like to know more about..'
                                     required
                                 />  
+                                <ValidationError prefix='Message' field='message' errors={state.errors} />
                             </div>
 
-                            <button type='submit'
+                            <button 
+                                type='submit'
+                                disabled={state.submitting}
                                 className='px-6 py-3 bg-dg/90 text-white font-semibold rounded-lg hover:bg-dg transition w-full cursor-pointer'
                             >
-                                Submit
+                                {state.submitting ? 'Sending...' : 'Submit'}
                             </button>
                         </form>
                     </div>
