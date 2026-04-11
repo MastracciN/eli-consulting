4098d030f4a8e4ac5de22b710ce684fb80cfffe5
Mastracci <38020285+MastracciN@users.noreply.github.com>
2026-04-11 15:25:08 -0400

    compressed forest-hero, canoe, and both logos to improve site performance

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/blackbird-header-dark.webp";
 export default function Footer() {
 
     return (
-        <footer className="py-16 min-h-[250px] bg-text text-white">
+        <footer className="py-16 min-h-[250px] bg-[#191c1b] text-white">
             <div className="max-w-[1400px] flex flex-col md:flex-row md:mx-auto">
                 <div className="flex mx-auto md:w-2/3 md:mx-0 px-2">
                     <h2 className="sr-only">Blackbird Bimaadiziwin Consulting</h2>
